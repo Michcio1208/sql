3e7c817900bc3ccc476648cd7637d4ca1084b07f
Michcio1208 <93605739+Michcio1208@users.noreply.github.com>
2024-04-03 14:10:25 +0200

    Add files via upload

--- drzewa/dodaj_ulice.php
+++ drzewa/dodaj_ulice.php
@@ -1,5 +1,10 @@
 <?php
 
+$ulica=$_POST["ulica"];
+$polaczenie=new mysqli("localhost","root","","20.03.2024");
+$polaczenie->query("INSERT INTO ulice(ulica) VALUES ('$ulica')");
+$polaczenie->close();
+echo "Dodano ulice $ulica <br/>";
 
 
 
